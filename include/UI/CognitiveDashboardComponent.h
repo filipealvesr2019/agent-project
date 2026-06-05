b1908c83da37547c174477fa3db25dd30ccf1f51
filipealvesrds2019 <filipealvesrds2019@gmail.com>
2026-06-05 10:09:03 -0300

    add

--- include/UI/CognitiveDashboardComponent.h
+++ include/UI/CognitiveDashboardComponent.h
@@ -15,6 +15,7 @@ public:
     // FileDragAndDropTarget
     bool isInterestedInFileDrag(const juce::StringArray& files) override;
     void fileDragEnter(const juce::StringArray& files, int x, int y) override;
+    void fileDragExit(const juce::StringArray& files) override;
     void filesDropped(const juce::StringArray& files, int x, int y) override;
 
     std::function<void(const juce::String&, const juce::String&)> onNavigateToWorkspace;
